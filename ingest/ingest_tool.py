c1f43f99c8f65d5afbed55bf2bb2543d7926c76d
Riley Chapman <Riley>
2024-06-16 21:09:48 -0700

    major updates to the ingest tool, can now pull card data

--- ingest/ingest_tool.py
+++ ingest/ingest_tool.py
@@ -2,21 +2,60 @@
 
 import argparse
 from ingest.get_expansions import get_expansions
-from ingest.serialize_data import unserialize
+from ingest.retrieve_card_data import get_card_data, get_kcard_links
+from ingest.serialize_data import serialize, unserialize
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Welcome to the DominionData ingest tool!')
-  parser.add_argument('-e','--get-expansion-data', default=False, action='store_true', help='Download Expansions data', required=False)
-  parser.add_argument('-s','--show', choices=['expansions', 'cards'], nargs="+", help='Show stored values for the provided choices', required=False)
-
+  parser.add_argument('-e','--get-expansion-data', action='store_true', help='Download Expansions data')
+  parser.add_argument('-E','--show-expansions', action='store_true', help='Show the stored list of sets')
+  parser.add_argument('-C','--show-cards', help='Show the cards for a specified expansion')
+  parser.add_argument('-l','--long', action='store_true', help='long output for show commands')
+  parser.add_argument('-c','--get-card-data', nargs="*", help='Download card data for specified expansions, or all if no argument is provided')
+  parser.add_argument('-d', action='store_true')
   args = parser.parse_args()
-  
+  if args.d:
+    print(args)
+    exit()
+
   if args.get_expansion_data: 
     print('get_expansion_data specified. Downloading all Dominion sets.')
     get_expansions(save_output=True)
     print('success')
 
-  for option in args.show:
-    print("Showing stored data for: ", option)
-    print(unserialize(option))
+  if args.get_card_data is not None:
+    requested_expansions = args.get_card_data
+    expansion_links = unserialize('expansions')
+    if requested_expansions == []: requested_expansions = list(expansion_links.keys())  #defualts to all
+    print('get_card_data specified. Downloading kingdom cards for sets: {}'.format(requested_expansions))
+    for expansion_name in requested_expansions:
+      link = expansion_links[expansion_name]
+      card_links = get_kcard_links(link)
+      print('downloading data for {}: '.format(expansion_name), end='')
+      expansion_data = {}
+      for card_link in card_links:
+        print('.', end='')
+        card_data = get_card_data(card_link)
+        expansion_data[card_data['name']] = card_data
+      print('')
+      serialize(expansion_data, 'sets/{}'.format(expansion_name.replace(' ', '_')))
+
+
+
+  if args.show_expansions:
+    print("Showing stored set list")
+    expansions = unserialize('expansions')
+    for name, link in expansions.items():
+      print('{} ({})'.format(name, link))
+
+  if args.show_cards:
+    expansions = args.show_cards
+    print("Showing cards for expansion: {}".format(expansions))
+    expansion_cards = unserialize('sets/{}'.format(expansions))
+    print('there are {} kingdom cards in this set:'.format(len(expansion_cards)))
+    for card in expansion_cards.values():
+      if args.long:
+        print(list(card.values()))
+      else: print(card['name'], end=", ")
+      print()
   #print('expansions:\n\n', expansions)
