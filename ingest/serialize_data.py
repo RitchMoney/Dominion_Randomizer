2bbe7ce42e91a049764da69f6ec51e6390a5855c
Riley Chapman <Riley>
2024-06-16 21:09:23 -0700

    create paths if needed

--- ingest/serialize_data.py
+++ ingest/serialize_data.py
@@ -1,11 +1,13 @@
 import pickle
+import os 
 
 def serialize(data : any, relative_path : str, base_path='dominion_data/pickle/') -> None:
-   path = '{}{}'.format(base_path, relative_path)
+   path = '{}{}.pkl'.format(base_path, relative_path)
+   os.makedirs(os.path.dirname(path), exist_ok=True) #creates dirs if needed
    with open (path, 'wb') as file:
     pickle.dump(data, file)
 
 def unserialize(pickle_name : str, base_path='dominion_data/pickle/') -> any:
-   path = '{}{}'.format(base_path, pickle_name)
+   path = '{}{}.pkl'.format(base_path, pickle_name)
    with open (path.format(path), 'rb') as f:
     return pickle.load(f)
